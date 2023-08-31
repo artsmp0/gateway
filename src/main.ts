d8e524af55d619b78a87520134027fec0694cd75
沈孟平 <artsmp@126.com>
2023-08-31 10:44:23 +0800

    自定义日志

--- src/main.ts
+++ src/main.ts
@@ -13,13 +13,19 @@ import { TransformInterceptor } from './common/interceptors/transform.intercepto
 import { AllExceptionFilter } from './common/exceptions/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
 import { generateDocument } from './doc';
+import fastify from 'fastify';
+import { FastifyLogger } from './common/logger';
 /*
 Fastify 在 QPS（并发请求处理） 的的效率远高于其他框架
  */
 async function bootstrap() {
+  const fastifyInstance: any = fastify({
+    logger: FastifyLogger,
+  });
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter(fastifyInstance),
   );
 
   // 接口版本化管理
